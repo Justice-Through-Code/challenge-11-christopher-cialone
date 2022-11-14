42e9f5ccc23ced68ccf5a4c191308087b565b32d
Christopher Cialone <ccialone0929@gmail.com>
2022-11-14 11:23:35 -0500

    almost

--- parse_laptop_data.py
+++ parse_laptop_data.py
@@ -66,10 +66,18 @@ laptops = [
 
 
 # 1.1 TODO: Print out the MacBook Pro url
-
+print(laptops[0]['url'])
 
 # 1.2 TODO: Write a function called `print_laptop_data` that takes in two parameters: `laptop` and `topic`, and returns nothing.
-#
+def print_laptop_data(laptop, topic):
+    computer = None
+    if laptop == 'Apple Macbook Pro':
+        computer = (laptops[0])
+    elif laptop == 'Dell XPS':
+        computer = (laptops[1])
+    else:
+        return
+    print(f'{laptop} {topic}: {computer["types"][0][topic]}')
 #   If `laptop` is 'Apple Macbook Pro', the function should print out data about that computer.
 #   If `laptop` is 'Dell XPS', the function should print out data about that computer.
 #
@@ -92,12 +100,18 @@ laptops = [
 #   1.3.1: All possible prices of the Apple Macbook Pro.
 #   1.3.2: All the color options for the Dell XPS.
 #   1.3.3: The screen_size of the Dell XPS.
-
+print_laptop_data('Apple Macbook Pro', 'price')
+print_laptop_data('Dell XPS', 'colors')
+print_laptop_data('Apple Macbook Pro', 'screen_size')
 
 # 2.1 TODO: Write a function called `list_prices` that takes one parameter: a list of computers, and returns nothing.
 #   Using nested loops, the function should print out all possible computer prices, one price on each line.
 #   No need to specify which computer each price belongs to.
-
+def list_prices(laptops):
+    for laptop in laptops:
+        for pc_laptop in laptop['types']:
+            for prices in pc_laptop['price']:
+                print(prices)
 
 # 2.2 TODO: Call your function to see that it works.
 
